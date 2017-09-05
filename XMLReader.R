b413fe09151606f2f38d01a16cec518318d3b076
HalforcNull <yaorunan@gmail.com>
2017-09-05 09:54:50 -0500

    xml read related test

---
+++ XMLReader.R
@@ -0,0 +1,18 @@
+#this test to prove we can use xml file as the config file
+#use with config.xml
+
+library("XML")
+
+node <- xmlNode("foo", attrs=c(a="1", b="my name"))
+
+doc <- xmlParse("config.xml")
+
+
+#the following provide the inner text of given xpath (a list of char)
+listOfChar <- xpathApply(doc, "//settings/testLoad", xmlValue)
+
+#this transfer list of char to string
+contentStr = paste( listOfChar[1]) 
+
+#this remove all newline symbol and return symbol
+e =gsub("[\r\n]","",t)
